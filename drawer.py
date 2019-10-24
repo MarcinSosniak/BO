bff7a4849fedd714d94532e6289d06d8479d9198
MarcinSosniak <marcinsosniakoff@gmail.com>
2019-10-24 15:56:42 +0200

    added colors for anthill and target

--- drawer.py
+++ drawer.py
@@ -2,7 +2,7 @@ import pygame
 
 class Drawer:
     @staticmethod
-    def draw(labirynth):
+    def draw(labirynth,cfg):
         pygame.init()
 
         gameDisplay = pygame.display.set_mode((640, 640))
@@ -25,17 +25,26 @@ class Drawer:
                     if elem is not None:
                         if elem > max:
                             max=elem
-
+            target_x=0
+            target_y=0
             for row in labirynth.get_lab():
                 for elem in row:
-                    color = (5, 25, 25) if elem is None else (int(elem/max*255), 100, 105)
+                    color = (0,0,0)
+                    if cfg.get_instance().anthill_pos.x() == target_x and cfg.get_instance().anthill_pos.y() == target_y:
+                        color= (150,75,0)
+                    elif cfg.get_instance().target_pos.x() == target_x and cfg.get_instance().target_pos.y() == target_y:
+                        color = (0,255,0)
+                    else:
+                        color = (5, 25, 25) if elem is None else ((128,100,105) if elem *2 < max and elem> 0.01*max else (int(elem/max*255), 100, 105))
                         # line.append(str(int(elem/max*9)))
                     
                     pygame.draw.rect(gameDisplay, color, pygame.Rect(x, y, size, size))
                     x += size
-                
+                    target_x+=1
                 x=0
                 y += size
+                target_y+=1
+                target_x=0
 
             pygame.display.update()
             clock.tick(60)
